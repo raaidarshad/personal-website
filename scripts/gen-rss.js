c32c8e91aa599f5f6e570a37d6c764dea2ef3a63
Raaid <raaid@protonmail.com>
2024-06-26 17:31:01 -0400

    convert import style

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -1,7 +1,7 @@
-const { promises: fs } = require('fs')
-const path = require('path')
-const RSS = require('rss')
-const matter = require('gray-matter')
+import { promises as fs } from 'fs'
+import { join } from 'path'
+import RSS from 'rss'
+import matter from 'gray-matter'
 
 async function generate() {
   const feed = new RSS({
@@ -10,14 +10,14 @@ async function generate() {
     feed_url: 'https://raaid.xyz/feed.xml'
   })
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
+  const posts = await fs.readdir(join(__dirname, '..', 'pages', 'posts'))
 
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
 
       const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
+        join(__dirname, '..', 'pages', 'posts', name)
       )
       const frontmatter = matter(content)
 
